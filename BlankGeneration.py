f387cf086c57ef6c0ce85d8b529c3db1aea8e4f0
zwb <2281862713@qq.com>
2020-08-15 11:17:54 +0800

    update BlankGeneration.py. 修改命令行实现，添加基本异常处理

--- BlankGeneration.py
+++ BlankGeneration.py
@@ -1,9 +1,21 @@
 #coding=utf-8
-import os
+# 异常处理
+import subprocess
 
-def blank(time, file_path):
-    cmd = "MKWav-7FH " + str(time * 1000) + " " + file_path  # 1000是把秒转换成毫秒为单位
-    os.system(cmd)
+Source_path = r"D:\项目\venv\Code\MKWav-7FH.exe "  # 命令行可执行文件的绝对路径
+
+# 命令行
+def blank(time, file_path):  # time是空白音频的时间（秒）
+    try:
+        cmd = Source_path + str(time * 1000) + " " + file_path  # 1000是把秒转换成毫秒为单位
+        res = subprocess.call(cmd, shell=True)  # 命令行
+
+        if res != 0:
+            return False
+        return True
+    except Exception:
+        return False
+
 
 #demo
-# blank(18, r"C:\Users\www22\Desktop\立项\备注提取\km10.wav")  # 第一个参数代表多少秒
+# blank(18, r"C:\Users\www22\Desktop\立项\备注提取\0.wav")
