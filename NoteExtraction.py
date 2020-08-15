6ee1250b484c0aa9c7b014cae7704d402c854da7
zwb <2281862713@qq.com>
2020-08-15 11:19:33 +0800

    update NoteExtraction.py.

--- NoteExtraction.py
+++ NoteExtraction.py
@@ -1,10 +1,10 @@
 from pptx import Presentation
 
-# 返回包含pptx备注的列表
+# 返回包含pptx文件备注的列表
 def ObtainPptNote(ppt_path):
     if ppt_path.strip() == '':  # 空路径,直接返回
         return []
-    obj = Presentation(ppt_path)  # 得到路径下的ppt文件对象
+    obj = Presentation(ppt_path)  # 得到路径下的PPT文件对象
     notes = []
     for index, slide in enumerate(obj.slides, start=1):
         text = slide.notes_slide.notes_text_frame.text  # 提取备注
