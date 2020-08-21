0b89a26c6672d2896bc813eb17cf2af4b535b494
zwb <2281862713@qq.com>
2020-08-21 19:57:35 +0800

    update VideoGeneration.py. 新增音量调节（可不用）

--- VideoGeneration.py
+++ VideoGeneration.py
@@ -6,10 +6,10 @@ from re import findall
 from win32com.client import gencache, Dispatch
 from moviepy.editor import *
 
-# ppt所在路径， wav音频所在路径， mp4视频输出路径， 时间序列
-def video(ppt_fn, wav_path, final_video_path, time):
+# ppt所在路径，wav音频所在路径，mp4视频输出路径，时间序列
+def video(ppt_fn, wav_path, final_video_path, time, volume):
     # 要转换的ppt文件
-    # ppt_fn = r'C:\Users\www22\Desktop\立项\备注提取\demo.pptx'  # 单引号里面是绝对路径(.pptx或.ppt)
+    # ppt_fn = r'C:\Users\www22\Desktop\立项\备注提取\大学生创新创业训练计划及学科竞赛.pptx'  # 单引号里面是绝对路径(.pptx或.ppt)
     # 临时存放拆分后的幻灯片的文件夹，新建存放每页ppt截图的文件夹
     pictures_dir = splitext(ppt_fn)[0] + '_pictures'  # 等号右边第一个是文件名(去除扩展名)
 
@@ -43,8 +43,17 @@ def video(ppt_fn, wav_path, final_video_path, time):
     # wav_path = r"C:\Users\www22\Desktop\立项\备注提取\output.wav"
     # final_video_path = r"C:\Users\www22\Desktop\立项\备注提取\output233.mp4"
     wav_clip = AudioFileClip(wav_path)
+
+    # 调整音量
+    if volume != 1:
+        wav_clip = wav_clip.volumex(volume)
     result_video = result_video.set_audio(wav_clip)
     result_video.write_videofile(final_video_path, fps = 24)  # 保存mp4视频，帧率24
 
     # 删除临时生成的文件夹
     rmtree(pictures_dir)
+
+# demo
+# time = [3.81968253968254, 2.728344671201814, 3.239183673469388, 1.822766439909297, 2.972154195011338, 1, 1.2190476190476192, 1, 1, 1.4280272108843537, 2.786394557823129, 1.3003174603174603, 1, 1, 1.4628571428571429, 1.2770975056689342, 1.9156462585034013, 1.3351473922902495, 3.4946031746031747, 1, 1, 1, 3.622312925170068, 3.7616326530612243, 4.400181405895691, 4.458231292517007, 2.6354648526077096, 3.5874829931972787, 3.5526530612244898, 5.955918367346939, 2.75156462585034, 2.9373242630385485, 5.572789115646258, 4.2260317460317465, 2.3336054421768706, 2.136235827664399]
+# volume = 5
+# video(r'C:\Users\www22\Desktop\立项\备注提取\大学生创新创业训练计划及学科竞赛.pptx', r"C:\Users\www22\Desktop\立项\备注提取\大学生创新创业训练计划及学科竞赛_wav\output1x.wav", r"C:\Users\www22\Desktop\立项\备注提取\output50.mp4", time, volume)
